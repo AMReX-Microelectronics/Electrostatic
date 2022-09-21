9972dcb592fea7b6893d211b2498edb0d3982b11
saurabh <SaurabhSawant@lbl.gov>
2022-09-21 07:52:14 -0700

    Branch commit: add parser_modification started

--- Source/Input/MaterialProperties.cpp
+++ Source/Input/MaterialProperties.cpp
@@ -1,66 +1,60 @@
-#include "Input.H"
+#include "MaterialProperties.H"
 
-void ReadBasicInput(GpuArray<int, AMREX_SPACEDIM>& n_cell, 
-                    int& max_grid_size, 
-                    GpuArray<Real, AMREX_SPACEDIM>& prob_lo, 
-                    GpuArray<Real, AMREX_SPACEDIM>& prob_hi, 
-                    int& nstep, 
-                    int& plot_int,
-                    int& plot_elems)
+using namespace amrex;
+
+MaterialProperties::MaterialProperties()
 {
-    // ParmParse is way of reading inputs from the inputs file
-    // pp.get means we require the inputs file to have it
-    // pp.query means we optionally need the inputs file to have it - but we must supply a default here
-    ParmParse pp;
+    ReadParameters();
+} 
 
-    amrex::Vector<int> temp_int(AMREX_SPACEDIM);
-    if (pp.queryarr("n_cell",temp_int)) {
-        for (int i=0; i<AMREX_SPACEDIM; ++i) {
-            n_cell[i] = temp_int[i];
-        }
+
+void Store_parserString(const amrex::ParmParse& pp, std::string query_string,
+                        std::string& stored_string)
+{
+    std::vector<std::string> f;
+    pp.getarr(query_string.c_str(), f);
+    stored_string.clear();
+    for (auto const& s : f) {
+        stored_string += s;
     }
-
-    pp.get("max_grid_size",max_grid_size);
-
-    amrex::Vector<Real> temp(AMREX_SPACEDIM);
-    if (pp.queryarr("prob_lo",temp)) {
-        for (int i=0; i<AMREX_SPACEDIM; ++i) {
-            prob_lo[i] = temp[i];
-        }
-    }
-
-    if (pp.queryarr("prob_hi",temp)) {
-        for (int i=0; i<AMREX_SPACEDIM; ++i) {
-            prob_hi[i] = temp[i];
-        }
-    }
-
-    nstep = 10;
-    pp.query("nstep",nstep);
-
-    plot_int = -1;
-    pp.query("plot_int",plot_int);
-
-    pp.get("plot_elems",plot_elems);
+    f.clear();
 }
 
-void ReadMaterialInput(Real& eps_0,
-                       Real& eps_r)
+void MaterialProperties::ReadParameters()
 {
     ParmParse pp;
-    std::unique_ptr<amrex::MultiFab> m_eps_mf;
-    std::string m_epsilon_s = "constant";
-    std::string m_str_eps_function;
-
+    /** permittivity (epsilon) */
+    bool epsilon_specified = false;
+    if (queryWithParser(pp, "epsilon", m_epsilon)) {
+        m_epsilon_s = "constant";
+        epsilon_specified = true;
+    }
     if (pp_macroscopic.query("epsilon_function(x,y,z)", m_str_epsilon_function) ) {
         m_epsilon_s = "parse_epsilon_function";
-        m_epsilon_specified = true;
+        epsilon_specified = true;
     }
+    if (!epsilon_specified) {
+        std::stringstream warnMsg;
+        warnMsg << "Material permittivity is not specified. Using default vacuum value of " 
+                <<  m_epsilon 
+                << " in the simulation.";
+        WarpX::GetInstance().RecordWarning("Material properties", warnMsg.str());
+    }
+
+    /** initialization of permittivity with a parser */
     if (m_epsilon_s == "parse_epsilon_function") {
-        Store_parserString(pp_macroscopic, "epsilon_function(x,y,z)", m_str_epsilon_function);
+        Store_parserString(pp, "epsilon_function(x,y,z)", m_str_epsilon_function);
         m_epsilon_parser = std::make_unique<amrex::Parser>(
                                  makeParser(m_str_epsilon_function,{"x","y","z"}));
     }
+}
+
+void
+MacroscopicProperties::InitData ()
+{
+    /** permittivity is stored at cell-centers */
+    m_eps_mf = std::make_unique<amrex::MultiFab>(ba, dm, Ncomp1, Nghost1); 
+
     // Initialize epsilon
     if (m_epsilon_s == "constant") {
 
@@ -71,27 +65,11 @@ void ReadMaterialInput(Real& eps_0,
         InitializeMacroMultiFabUsingParser(m_eps_mf.get(), m_epsilon_parser->compile<3>(), lev);
 
     }
-    // Material Properties
-    pp.get("eps_0",eps_0); // permittivity of vaccum
-    pp.get("eps_r",eps_r); // relative permittivity of the material
 }
 
-void ReadMLMGInput(Real& ascalar,
-                   Real& bscalar,
-                   int& set_verbose,
-                   int& max_order)
-{
-    ParmParse pp;
-    pp.get("mlmg_ascalar",ascalar);
-    pp.get("mlmg_bscalar",bscalar);
-    set_verbose = 0;
-    pp.query("mlmg_set_verbose",set_verbose);
-    max_order = 2;
-    pp.query("mlmg_max_order",max_order);
-}
 
 void
-MacroscopicProperties::InitializeMacroMultiFabUsingParser (
+MaterialProperties::InitializeMacroMultiFabUsingParser (
                        amrex::MultiFab *macro_mf,
                        amrex::ParserExecutor<3> const& macro_parser,
                        const int lev)
