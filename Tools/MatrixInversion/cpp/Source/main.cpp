172da072392af446102a8577d13607ece0cadf0f
Saurabh Sawant <SaurabhSawant@lbl.gov>
2023-02-24 14:47:57 -0800

    replaced gpuarrays with table

--- Tools/MatrixInversion/cpp/Source/main.cpp
+++ Tools/MatrixInversion/cpp/Source/main.cpp
@@ -58,68 +58,62 @@ void PrintTable(const TableData<MatrixDType, 2>& G)
 }
 
 template<std::size_t N>
-void MatInv_BlockTriDiagonal(amrex::GpuArray<amrex::GpuComplex<amrex::Real>, N> A, 
-                             amrex::GpuArray<amrex::GpuComplex<amrex::Real>, N-1> B, 
-                             amrex::GpuArray<amrex::GpuComplex<amrex::Real>, N-1> C,
-                             TableData<MatrixDType, 2>& G)
+void MatInv_BlockTriDiagonal(TableData<MatrixDType, 1>& A_data, 
+                             TableData<MatrixDType, 1>& B_data, 
+                             TableData<MatrixDType, 1>& C_data,
+                             TableData<MatrixDType, 2>& G_data)
 {
-    amrex::GpuArray<amrex::GpuComplex<amrex::Real> , N> X_tilde, Y_tilde;
-    amrex::GpuArray<amrex::GpuComplex<amrex::Real>, N> X, Y;
+    TableData<MatrixDType, 1> X_tilde_data({0},{N});
+    TableData<MatrixDType, 1> Y_tilde_data({0},{N});
+    TableData<MatrixDType, 1> X_data({0},{N});
+    TableData<MatrixDType, 1> Y_data({0},{N});
 
+    auto const& X_tilde = X_tilde_data.table();
+    auto const& Y_tilde = Y_tilde_data.table();
+    auto const& X = X_data.table();
+    auto const& Y = Y_data.table();
+    auto const& A = A_data.const_table();
+    auto const& B = B_data.const_table();
+    auto const& C = C_data.const_table();
 
-    Y[0] = 0;
+    Y(0) = 0;
     for (int n = 1; n < N; ++n)
     {   
-        Y_tilde[n] = C[n-1]/(A[n-1] - Y[n-1]);
-        Y[n] = B[n-1]*Y_tilde[n];
+	int p = (n-1)%2;    
+        Y_tilde(n) = C(p) / ( A(n-1) - Y(n-1) );
+        Y(n) = B(p) * Y_tilde(n);
     }
     //amrex::Print() << "\nY: \n";
     //for (std::size_t n = 1; n < N; ++n)
     //{   
-    //    amrex::Print() << Y_tilde[n] << "\n";
+    //    amrex::Print() << Y_tilde(n) << "\n";
     //}
 
-    X[N-1] = 0;
+    X(N-1) = 0;
     for (int n = N-2; n > -1; n--)
     {   
-        X_tilde[n] = B[n]/(A[n+1] - X[n+1]);
-        X[n] = C[n]*X_tilde[n];
+	int p = n%2;    
+        X_tilde(n) = B(p)/(A(n+1) - X(n+1));
+        X(n) = C(p)*X_tilde(n);
     }
     //amrex::Print() << "\nX: \n";
     //for (int n = N-2; n > -1; n--)
     //{   
-    //    amrex::Print() << X_tilde[n] << "\n";
+    //    amrex::Print() << X_tilde(n) << "\n";
     //}
 
-    //auto const& h_table = G.table();
-    //for (int n = 0; n < N; ++n) /*This loop can be parallelized*/
-    //{
-    //    h_table(n,n) = 1./(A[n] - X[n] - Y[n]);
-    //}
-    //for (int n = 0; n < N; ++n) /*This loop can be parallelized*/
-    //{   
-    //    for (int m = n; m > 0; m--)
-    //    {   
-    //        h_table(m-1,n) = -Y_tilde[m]*h_table(m,n);
-    //    }
-    //    for (int m = n; m < N-1; ++m)
-    //    {   
-    //        h_table(m+1,n) = -X_tilde[m]*h_table(m,n);
-    //    }
-    //}
-
-    auto const& table = G.table();
-    amrex::ParallelFor(N, [table=table,A=A, X=X, Y=Y,Y_tilde=Y_tilde,X_tilde=X_tilde] AMREX_GPU_DEVICE (int n) noexcept
+    auto const& G = G_data.table();
+    amrex::ParallelFor(N, [=] AMREX_GPU_DEVICE (int n) noexcept
     {
-        table(n,n) =  1./(A[n] - X[n] - Y[n]); 
+        G(n,n) =  1./(A(n) - X(n) - Y(n)); 
 
         for (int m = n; m > 0; m--)
         {   
-            table(m-1,n) =  -Y_tilde[m]*table(m,n);
+            G(m-1,n) =  -Y_tilde(m)*G(m,n);
         }
         for (int m = n; m < N-1; ++m)
         {   
-            table(m+1,n) = -X_tilde[m]*table(m,n);
+            G(m+1,n) = -X_tilde(m)*G(m,n);
         }
     });
     amrex::Gpu::streamSynchronize();
@@ -134,12 +128,18 @@ int main (int argc, char* argv[])
     //Construct Tridiagonal Dummy Hamiltonian
     std::array<amrex::Real,3> point_charge_loc {0., 0., 1e-9};
 
-    const int N = 5;
-    amrex::GpuArray<amrex::GpuComplex<amrex::Real>, N  > A; //diagonal
-    amrex::GpuArray<amrex::GpuComplex<amrex::Real>, N-1> B; //+1 diagonal
-    amrex::GpuArray<amrex::GpuComplex<amrex::Real>, N-1> C; //-1 diagonal
+    const int N = 30000;
+    const int P = 2;
+    amrex::Real gamma = 2.5; //eV
+    int M=17;
 
-    //amrex::Print() << "\nA & r: \n";
+    TableData<MatrixDType, 1> A_data({0},{N});
+    TableData<MatrixDType, 1> B_data({0},{P});
+    TableData<MatrixDType, 1> C_data({0},{P});
+    auto const& A = A_data.table();
+    auto const& B = B_data.table();
+    auto const& C = C_data.table();
+
     for (std::size_t i = 0; i < N; ++i)
     {
         amrex::Real layer_loc_y = -10e-9 + (static_cast<amrex::Real>(i)/(N-1))*20e-9;        
@@ -148,68 +148,37 @@ int main (int argc, char* argv[])
                               + pow((layer_loc_y - point_charge_loc[1]),2)
                               + pow((0.          - point_charge_loc[2]),2) ), 0.5);
 
-        A[i] = -(PhysConst::q_e)/(4. * MathConst::pi * PhysConst::ep0 * r);
-    //    amrex::Print() << i << std::setw(25) << A[i] << std::setw(25) << layer_loc_y << "\n"; 
+        A(i) = -(PhysConst::q_e)/(4. * MathConst::pi * PhysConst::ep0 * r);
     }
 
-    amrex::Real gamma = 2.5; //eV
-    int M=17;
     amrex::GpuComplex<amrex::Real> beta = get_beta(gamma,M,6);
 
-    //amrex::Print() << "\nB & C: \n";
-    for (std::size_t i = 0; i < N-1; ++i)
+    for (std::size_t i = 0; i < P; ++i)
     {
        if(i%2 == 0) {
-          B[i] = conjugate(beta);
-          C[i] = beta;
+          B(i) = conjugate(beta);
+          C(i) = beta;
        }
        else {
-          B[i] = gamma;
-          C[i] = gamma;
+          B(i) = gamma;
+          C(i) = gamma;
        } 
-    //   amrex::Print() << i << std::setw(25) << B[i] << std::setw(25) << C[i] << "\n"; 
     }
  
     Array<int,2> tlo{0,0};
     Array<int,2> thi{N-1,N-1};
 
-    //#ifdef AMREX_USE_GPU
-    //    TableData<MatrixDType,2> G_inv(tlo, thi, The_Pinned_Arena());
-    //#else
-    //    TableData<MatrixDType,2> G_inv(tlo, thi);
-    //#endif
-    //auto const& G_inv_table = G_inv.table();
-
-    //for (int j = tlo[1]; j <= thi[1]; ++j) { //running over columns
-    //for (int i = tlo[0]; i <= thi[0]; ++i) { //running over rows
-    //    G_inv_table(i,j) = 0.; //i + j*1e3;
-    //}}
-
-    //for (int i = tlo[0]; i <= thi[0]; ++i) {
-    //    G_inv_table(i,i)   = A[i];
-
-    //    if(i < thi[0]) 
-    //    {
-    //        G_inv_table(i,i+1) = C[i];
-    //        G_inv_table(i+1,i) = B[i];
-    //    }  
-    //}
-    ////TD<decltype(h_table)> h_table_type; //const amrex::Table2D<amrex::Real>&    
-    //amrex::Print() << "G_inv:\n";
-    //PrintTable(G_inv);
-
-
-    TableData<MatrixDType, 2> G(tlo,thi);
+    TableData<MatrixDType, 2> G_data(tlo,thi);
 
     amrex::Real mat_inv_beg_time = amrex::second();
 
-    MatInv_BlockTriDiagonal<N>(A,B,C,G);   
+    MatInv_BlockTriDiagonal<N>(A_data,B_data,C_data,G_data);   
 
     amrex::Real mat_inv_time = amrex::second() - mat_inv_beg_time;
     amrex::Print() << "Matrix inversion time: " << std::setw(15) << mat_inv_time << "\n";
 
-    amrex::Print() << "G:\n";
-    PrintTable(G);
+    //amrex::Print() << "G:\n";
+    //PrintTable(G_data);
  
     amrex::Finalize();
 
