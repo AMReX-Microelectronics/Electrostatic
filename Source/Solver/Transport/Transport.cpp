3fa227557f179cd280aa7c9ce8b0965b080aae55
Saurabh@Home <SaurabhSawant@lbl.gov>
2023-07-21 15:26:49 -0700

    Broyden's first algorithm added

--- Source/Solver/Transport/Transport.cpp
+++ Source/Solver/Transport/Transport.cpp
@@ -328,6 +328,11 @@ c_TransportSolver::Solve(const int step, const amrex::Real time)
 
             switch(map_AlgorithmType[Algorithm_Type])
             {
+                case s_Algorithm::Type::broyden_first:
+                {
+	            Execute_Broyden_First_Algorithm(); 
+                    break;
+                }
                 case s_Algorithm::Type::broyden_second:
                 {
 	            Execute_Broyden_Modified_Second_Algorithm(); 
@@ -439,6 +444,36 @@ c_TransportSolver:: Set_Broyden ()
         SetVal_RealTable1D(delta_F_curr_data, 0.);
         SetVal_RealTable1D(delta_n_curr_data, 0.);
 
+
+        switch(map_AlgorithmType[Algorithm_Type])
+        {
+            case s_Algorithm::Type::broyden_first:
+            {
+                Jinv_curr_data.resize({0,0},{num_field_sites_all_NS, num_field_sites_all_NS}, The_Pinned_Arena());
+                SetVal_RealTable2D(Jinv_curr_data,0.);
+
+                auto const& Jinv_curr    = Jinv_curr_data.table();
+                for(int a=0; a < num_field_sites_all_NS; ++a) 
+                {
+                    Jinv_curr(a,a) = Broyden_fraction;
+                }
+                break;
+            }
+            case s_Algorithm::Type::broyden_second:
+            {
+                break;
+            }
+            case s_Algorithm::Type::simple_mixing:
+            {
+                break;
+            }
+            default:
+            {
+                amrex::Abort("In Set_Broyden: selfconsistency_algorithm, " + Algorithm_Type + ", is not yet defined.");
+            }
+        }
+	
+	
         amrex::Print() << "\nBroyden parameters are set to the following: \n";
         amrex::Print() << " Broyden_Step: "                          << Broyden_Step                          << "\n";
         amrex::Print() << " Broyden_Scalar: "                        << Broyden_Scalar                        << "\n";
@@ -491,7 +526,32 @@ c_TransportSolver:: Reset_Broyden ()
         SetVal_RealTable1D(delta_F_curr_data, 0.);
         SetVal_RealTable1D(delta_n_curr_data, 0.);
 
-        //SetVal_Table2D(Jinv_curr_data,0.);
+        switch(map_AlgorithmType[Algorithm_Type])
+        {
+            case s_Algorithm::Type::broyden_first:
+            {
+                //SetVal_RealTable2D(Jinv_curr_data,0.);
+
+                //auto const& Jinv_curr    = Jinv_curr_data.table();
+                //for(int a=0; a < num_field_sites; ++a) 
+                //{
+                //    Jinv_curr(a,a) = Broyden_fraction;
+                //}
+                break;
+            }
+            case s_Algorithm::Type::broyden_second:
+            {
+                break;
+            }
+            case s_Algorithm::Type::simple_mixing:
+            {
+                break;
+            }
+            default:
+            {
+                amrex::Abort("In Set_Broyden: selfconsistency_algorithm, " + Algorithm_Type + ", is not yet defined.");
+            }
+        }
 
         Broyden_Step = 1;
         Broyden_Norm = 1;
@@ -584,6 +644,169 @@ c_TransportSolver:: Restart_Broyden ()
         amrex::Print() << " Broyden_Max_Norm: "         << Broyden_Norm << "\n\n";
 
     }
+}
+
+
+void 
+c_TransportSolver:: Execute_Broyden_First_Algorithm ()
+{
+
+    if (ParallelDescriptor::IOProcessor())
+    {
+        amrex::Print() << "\nBroydenStep: " << Broyden_Step  << ",  fraction: " << Broyden_fraction << ",  scalar: " << Broyden_Scalar<< "\n";
+	
+        auto const& n_curr_in  = n_curr_in_data.table();
+        auto const& n_curr_out = n_curr_out_data.table();
+        auto const& n_prev_in  = n_prev_in_data.table();
+        auto const& F_curr     = F_curr_data.table();
+        auto const& delta_F_curr  = delta_F_curr_data.table();
+        auto const& delta_n_curr   = delta_n_curr_data.table();
+        auto const& Jinv_curr    = Jinv_curr_data.table();
+
+        RealTable1D sum_Fcurr_data({0},{num_field_sites_all_NS}, The_Pinned_Arena());
+        RealTable1D sum_deltaFcurr_data({0},{num_field_sites_all_NS}, The_Pinned_Arena());
+        RealTable1D delta_n_Jinv_data({0},{num_field_sites_all_NS}, The_Pinned_Arena());
+
+        auto const& sum_Fcurr      = sum_Fcurr_data.table();
+        auto const& sum_deltaFcurr = sum_deltaFcurr_data.table();
+        auto const& delta_n_Jinv   = delta_n_Jinv_data.table();
+
+        SetVal_RealTable1D(Norm_data, 0.);
+        auto const& Norm       = Norm_data.table();
+
+        Broyden_NormSum_Curr = 0.;
+
+        for(int l=0; l < num_field_sites_all_NS; ++l)
+        {
+            sum_Fcurr(l) = 0;
+            sum_deltaFcurr(l) = 0;
+            delta_n_Jinv(l) = 0.;
+        }
+
+        switch(map_NormType[Broyden_Norm_Type])
+        {
+            case s_Norm::Type::Absolute:
+            {
+                for(int l=0; l < num_field_sites_all_NS; ++l)
+                {
+                    amrex::Real Fcurr = n_curr_in(l) - n_curr_out(l);
+                    Norm(l) = fabs(Fcurr);
+                    Broyden_NormSum_Curr += pow(Fcurr,2);
+                }
+                break;
+            }
+            case s_Norm::Type::Relative:
+            {
+                for(int l=0; l < num_field_sites_all_NS; ++l)
+                {
+                    amrex::Real Fcurr = n_curr_in(l) - n_curr_out(l);
+                    Norm(l) = fabs(Fcurr/(n_curr_in(l) + n_curr_out(l)));
+                    Broyden_NormSum_Curr += pow(Norm(l),2);
+                }
+                break;
+            }
+            default:
+            {
+                amrex::Abort("Norm Type " + Broyden_Norm_Type + " is not yet defined.");
+            }
+        }
+        Broyden_NormSum_Curr = sqrt(Broyden_NormSum_Curr);
+
+        Broyden_Norm = Norm(0);
+        int norm_index = 0;
+        for(int l=1; l < num_field_sites_all_NS; ++l)
+        {
+            if(Broyden_Norm < Norm(l))
+            {
+                Broyden_Norm = Norm(l);
+                norm_index = l;
+            }
+        }
+        amrex::Print() << "\nBroyden_NormSum_Curr: " << std::setw(20) << Broyden_NormSum_Curr << "\n";
+        amrex::Print() <<   "Broyden_NormSum_Prev: " << std::setw(20) << Broyden_NormSum_Prev
+                       << ",   Difference: " << (Broyden_NormSum_Curr - Broyden_NormSum_Prev) << "\n";
+        amrex::Print() << "Broyden max norm: " << Broyden_Norm << " at location: " <<  norm_index << "\n\n";
+
+
+        Broyden_NormSum_Prev = Broyden_NormSum_Curr;
+
+        for(int l=0; l < num_field_sites_all_NS; ++l)
+        {
+            amrex::Real Fcurr = n_curr_in(l) - n_curr_out(l);
+            delta_F_curr(l) = Fcurr - F_curr(l);
+            F_curr(l) = Fcurr;
+            delta_n_curr(l) = n_curr_in(l) - n_prev_in(l);
+        }
+
+        amrex::Print() << "n_curr_in, n_prev_in: " << n_curr_in(0) << " " << n_prev_in(0) << "\n";
+
+        int m = Broyden_Step-1;
+        if(m > 0 ) 
+	{
+            for(int a=0; a < num_field_sites_all_NS; ++a)
+            {
+                amrex::Real sum = 0.;
+                for(int b=0; b < num_field_sites_all_NS; ++b)
+                {
+                    sum += Jinv_curr(a,b)*delta_F_curr(b);
+                }
+                sum_deltaFcurr(a) = sum;
+            }
+
+	    amrex::Real denom = 0.;
+            for(int l=0; l < num_field_sites_all_NS; ++l)
+            {
+                denom += delta_n_curr(l) * sum_deltaFcurr(l);
+            }
+
+            for(int b=0; b < num_field_sites_all_NS; ++b)
+            {
+                amrex::Real sum = 0.;
+                for(int a=0; a < num_field_sites_all_NS; ++a)
+                {
+                    sum += delta_n_curr(a)*Jinv_curr(a,b);
+                }
+                delta_n_Jinv(b) = sum;
+            }
+
+            for(int a=0; a < num_field_sites_all_NS; ++a)
+            {
+                for(int b=0; b < num_field_sites_all_NS; ++b)
+                {
+                    Jinv_curr(a,b) += ( delta_n_curr(a) - sum_deltaFcurr(a) )  * delta_n_Jinv(b) / denom;
+                }
+            }
+        }
+        for(int a=0; a < num_field_sites_all_NS; ++a)
+        {
+            amrex::Real sum = 0.;
+            for(int b=0; b < num_field_sites_all_NS; ++b)
+            {
+                sum += Jinv_curr(a,b)*F_curr(b);
+            }
+            sum_Fcurr(a) = sum;
+        }
+
+        for(int l=0; l < num_field_sites_all_NS; ++l)
+        {
+            n_prev_in(l) = n_curr_in(l);
+            n_curr_in(l) = n_prev_in(l) - sum_Fcurr(l);
+        }
+        amrex::Print() << "n_new_in: " << n_curr_in(0) << "\n";
+
+        sum_Fcurr_data.clear();
+        sum_deltaFcurr_data.clear();
+        delta_n_Jinv_data.clear();
+
+        Broyden_Step += 1;
+    }
+
+    MPI_Bcast(&Broyden_Norm,
+               1,
+               MPI_DOUBLE,
+               ParallelDescriptor::IOProcessorNumber(),
+               ParallelDescriptor::Communicator());
+
 }
 
 
@@ -933,3 +1156,20 @@ c_TransportSolver:: SetVal_RealTable1D (RealTable1D& Tab1D_data, amrex::Real val
         Tab1D(i) = val;
     }
 }
+
+void 
+c_TransportSolver:: SetVal_RealTable2D (RealTable2D& Tab2D_data, amrex::Real val)
+{
+    auto tlo = Tab2D_data.lo();
+    auto thi = Tab2D_data.hi();
+    
+    auto const& Tab2D = Tab2D_data.table();
+    
+    for (int i = tlo[0]; i < thi[0]; ++i)
+    {
+        for (int j = tlo[1]; j < thi[1]; ++j) //slow moving index. printing slow
+        {
+            Tab2D(i,j) = val;
+        }
+    }
+}  
